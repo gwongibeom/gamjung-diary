63b3a8aeb77509bb0990b03708139d2bfd82e11d
gwongibeom <k201gun@kakao.com>
2024-04-09 20:05:21 +0900

    필터링 기능

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import DiaryList from '../components/DiaryList'
 
 const Home = () => {
-  const [params, setParams] = useSearchParams()
-  return <div>Home</div>
+  const [pivotDate, setPivotDate] = useState(new Date())
+
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={'<'} />}
+        rightChild={<Button text={'>'} />}
+      />
+      <DiaryList />
+    </div>
+  )
 }
 
 export default Home
