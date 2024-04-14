c974b7c3edc9445907ae2781fb54c0b450514631
gwongibeom <k201gun@kakao.com>
2024-04-14 22:43:02 +0900

    뒤로가기 코드 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,13 +1,15 @@
 import Header from '../components/Header.jsx'
 import Button from '../components/Button.jsx'
 import Editor from '../components/Editor.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const New = () => {
+  const nav = useNavigate()
   return (
     <div>
       <Header
         title={'새 일기 쓰기'}
-        leftChild={<Button text={'< 뒤로가기'} />}
+        leftChild={<Button text={'< 뒤로가기'} onClick={() => nav(-1)} />}
       />
       <Editor />
     </div>
